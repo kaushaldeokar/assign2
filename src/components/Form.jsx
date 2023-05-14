ef2bf76368df2be149af3f0aa1a56f727a61751c
kaushaldeokar <kaushal.deokar@gmail.com>
2023-05-14 19:43:21 +0530

    Form button link

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import RegisterData from "../pages/RegisterData"
-
+import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link';
 const Form = () => {
   return (
     <div>Form
-        <RegisterData/>
+      <RegisterData />
       
+    
+      <div>Corporate Information   <Link href="#"><EditIcon/></Link></div>
+      
+
+
+
     </div>
   )
 }
